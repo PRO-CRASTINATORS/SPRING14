d7936f29d47fc9a06bc037b3fd32ee186bc4c8bc
ewu-phillipsk <kphillips2010@eagles.ewu.edu>
2014-04-24 13:35:15 -0700

    Kyle's Test Branch!
    Updated kphillisp_helloworld.java slightly.

--- Assignment4GitHub/src/kphillips_helloworld.java
+++ Assignment4GitHub/src/kphillips_helloworld.java
@@ -3,5 +3,6 @@ public class kphillips_helloworld
    public static void main(String [] args)
    {
       System.out.println("Hello world!");
+      System.out.println("This is a test of the Branch feature!");
    }
 }//end
