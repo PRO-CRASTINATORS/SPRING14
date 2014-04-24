2dcb6a683cf2fac473187286a4937c51b29ad35b
ewu-phillipsk <kphillips2010@eagles.ewu.edu>
2014-04-24 13:39:17 -0700

    Revert "Kyle's Test Branch!"
    This reverts commit d7936f29d47fc9a06bc037b3fd32ee186bc4c8bc.

--- Assignment4GitHub/src/kphillips_helloworld.java
+++ Assignment4GitHub/src/kphillips_helloworld.java
@@ -3,6 +3,5 @@ public class kphillips_helloworld
    public static void main(String [] args)
    {
       System.out.println("Hello world!");
-      System.out.println("This is a test of the Branch feature!");
    }
 }//end
