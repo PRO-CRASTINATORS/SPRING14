c419bc8ae7971251ac5dd91bacdb3833464637f0
kevinMurr <kmurray@eagle.ewu.edu>
2014-04-24 18:21:34 -0700

    Updated a java file

--- Assignment4GitHub/src/KevinMurray_Hello.java
+++ Assignment4GitHub/src/KevinMurray_Hello.java
@@ -3,5 +3,9 @@ public class KevinMurray_Hello
    public static void main(String [] args)
    {
       System.out.println("Hello world!");
+		
+		System.out.println("Testing Branch features...");
+		
+		
    }
 }
