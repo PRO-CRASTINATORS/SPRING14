0979c55d0771064e9bd849ea305fe7c64e97d37d
kevinMurr <kmurray@eagle.ewu.edu>
2014-04-24 18:30:26 -0700

    Revert "Updated a java file"
    This reverts commit c419bc8ae7971251ac5dd91bacdb3833464637f0.

--- Assignment4GitHub/src/KevinMurray_Hello.java
+++ Assignment4GitHub/src/KevinMurray_Hello.java
@@ -3,9 +3,5 @@ public class KevinMurray_Hello
    public static void main(String [] args)
    {
       System.out.println("Hello world!");
-		
-		System.out.println("Testing Branch features...");
-		
-		
    }
 }
